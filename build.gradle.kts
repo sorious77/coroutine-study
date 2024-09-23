52a3efc4c8a068a5a3e9263570689382c6452718
sorious77 <sorious77@naver.com>
2024-09-23 22:27:09 +0900

    :wrench: 스프링 의존성 제거 및 코루틴 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.3.4"
-    id("io.spring.dependency-management") version "1.1.6"
+    kotlin("jvm") version "1.8.20"
 }
 
 group = "com.hoang"
@@ -10,7 +7,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 
@@ -18,18 +15,14 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+object Version {
+    const val COROUTINE = "1.9.0"
 }
 
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
-    }
+dependencies {
+    runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Version.COROUTINE}")
+
+    testImplementation(kotlin("test"))
 }
 
 tasks.withType<Test> {
---
+++ src/main/kotlin/com/hoang/coroutinestudy/Main.kt
@@ -0,0 +1,5 @@
+package com.hoang.coroutinestudy
+
+fun main() {
+
+}
